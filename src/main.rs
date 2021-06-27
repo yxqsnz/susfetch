a2a0b9dbadc1fd2ab9254d049813a65ed8ce057c
yxqsnz <bolaknt@gmail.com>
2021-06-27 17:07:53 -0300

    v1

---
+++ src/main.rs
@@ -0,0 +1,95 @@
+use std::env;
+
+use sysinfo::*;
+#[derive(Debug)]
+pub struct SusFetch {
+    kernel: Option<String>,
+    shell: String,
+    wm: String,
+    host: String,
+    os: String,
+}
+impl Default for SusFetch {
+    fn default() -> Self {
+        let sys = System::new_all();
+
+        let kernel = sys.get_kernel_version();
+
+        let host = format!("{}@{}", whoami::username(), whoami::hostname());
+        let os = whoami::distro();
+        let mut shell = "unknown".to_string();
+        if let Ok(var) = env::var("SHELL") {
+            let split: Vec<_> = var.split('/').collect();
+            if let Some(sh) = split.last() {
+                shell = sh.to_string();
+            }
+        }
+        let wm = env::var("DESKTOP_SESSION").unwrap_or_else(|_| "unknown".into());
+        Self {
+            kernel,
+            host,
+            os,
+            wm,
+            shell,
+        }
+    }
+}
+impl SusFetch {
+    fn format(&mut self) {
+        if self.kernel.is_none() {
+            self.kernel = Some(yansi::Paint::red("Linux (Failed to get kernel.)").to_string());
+            if self.wm == *"unknown" {
+                self.wm = yansi::Paint::red("unknown").to_string();
+            }
+
+            if self.shell == *"unknown" {
+                self.shell = yansi::Paint::red("unknown").to_string();
+            }
+        }
+    }
+    fn show(&self) {
+        println!(
+            r#"
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣤⣤⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀{}
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⠟⠉⠉⠉⠉⠉⠉⠉⠙⠻⢶⣄⠀⠀   -------⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣷⡀⠀⠀⠀{}: {}
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡟⠀⣠⣶⠛⠛⠛⠛⠛⠛⠳⣦⡀⠀⠘⣿⡄⠀⠀{}: {}
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⠁⠀⢹⣿⣦⣀⣀⣀⣀⣀⣠⣼⡇⠀⠀⠸⣷⠀⠀{}: {}
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡏⠀⠀⠀⠉⠛⠿⠿⠿⠿⠛⠋⠁⠀⠀⠀⠀⣿⡄ {}: {}
+⠀⠀    ⠀⠀⢠⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡇⠀
+      ⠀⠀⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡇⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣧⠀
+⠀⠀⠀⠀⠀⠀⠀⢸⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⠀
+⠀⠀⠀⠀⠀⠀⠀⣾⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀
+⠀⠀⠀⠀⠀⠀⠀⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠀
+⠀⠀⠀⠀⠀⠀⢰⣿⠀⠀⠀⠀⣠⡶⠶⠿⠿⠿⠿⢷⣦⠀⠀⠀⠀⠀⠀⠀⣿⠀
+⠀⠀⣀⣀⣀⠀⣸⡇⠀⠀⠀⠀⣿⡀⠀⠀⠀⠀⠀⠀⣿⡇⠀⠀⠀⠀⠀⠀⣿⠀
+⣠⡿⠛⠛⠛⠛⠻⠀⠀⠀⠀⠀⢸⣇⠀⠀⠀⠀⠀⠀⣿⠇⠀⠀⠀⠀⠀⠀⣿⠀
+⢻⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⡟⠀⠀⢀⣤⣤⣴⣿⠀⠀⠀⠀⠀⠀⠀⣿⠀
+⠈⠙⢷⣶⣦⣤⣤⣤⣴⣶⣾⠿⠛⠁⢀⣶⡟⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⠀
+          ⠀⠀⠀⠀⠈⣿⣆⡀⠀⠀⠀⠀⠀⠀⢀⣠⣴⡾⠃⠀
+⠀        ⠀⠀⠀⠀⠀⠀⠈⠛⠻⢿⣿⣾⣿⡿⠿⠟⠋⠁⠀⠀"#,
+            self.host,
+            yansi::Paint::green("OS"),
+            self.os,
+            yansi::Paint::green("Kernel"),
+            if let Some(kernel) = &self.kernel {
+                kernel
+            } else {
+                panic!("Err...")
+            },
+            yansi::Paint::green("Shell"),
+            self.shell,
+            yansi::Paint::green("WM"),
+            self.wm,
+        )
+    }
+    pub fn run(&mut self) {
+        self.format();
+        self.show();
+    }
+}
+fn main() {
+    let mut sus = SusFetch::default();
+    sus.run();
+}
